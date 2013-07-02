605a5b23abe682a0834dfde5c393b4b9f217cb62
Blake Taylor <blakefrost@gmail.com>
2013-07-02 14:23:36 -0700

    Remove YAML load, leave configuration to initializer

--- lib/smooch.rb
+++ lib/smooch.rb
@@ -10,18 +10,9 @@ module Smooch
 
 end
 
-begin
-  config = YAML.load_file("#{Rails.root}/config/kissmetrics.yml")
-  Smooch::API_KEY = config[Rails.env]['apikey'] if config[Rails.env]
-rescue
-  puts "Error opening KISSmetrics configuration file."
-end
-
 require 'smooch/base'
 require 'smooch/helpers'
 require 'smooch/controller'
-
-
 
 ActionController::Base.class_eval do
   extend Smooch::Controller
